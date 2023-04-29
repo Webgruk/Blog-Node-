d2e67fd1d2cfa2b8276417f6375796ff4fb1af4d
immanuel <immanuelajimah@gmail.com>
2023-04-29 07:07:25 +0100

    Created page routes

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
 
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'Pages/index.html'))
+})
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'Pages/about.html'))
+})
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'Pages/contact.html'))
+})
+app.get('/post', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'Pages/post.html'))
+})
+
 app.listen(4000, () => {
   console.log('App listening on port 4000')
 })
